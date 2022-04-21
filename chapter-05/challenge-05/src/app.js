f8a2b9ea8210263c83c2345db662733b1979ce3b
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-04-21 22:42:18 +0700

    add(challenge-05): api documentation

--- chapter-05/challenge-05/src/app.js
+++ chapter-05/challenge-05/src/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const swaggerUi = require("swagger-ui-express");
 
 const { authorization, errorHandling } = require("./api/middlewares");
 const { indexRouter, usersRouter, loginRouter } = require("./api/routes");
+const swaggerDocument = require("./api-docs/swagger.json");
 
 const app = express();
 
@@ -13,7 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "api/public")));
+app.use("/api-docs", swaggerUi.serve);
 
+app.get("/api-docs", swaggerUi.setup(swaggerDocument));
 app.use("/", indexRouter);
 app.use("/login", loginRouter);
 app.use(authorization);
